99f4e37d4b692893b2595c8201ce842f38c04d16
kitana-eh <kitana.ananda@gmail.com>
2018-06-05 15:28:34 -0400

    Created and modified two files

--- python/code_ex_1.py
+++ python/code_ex_1.py
@@ -1,7 +1,11 @@
 address = {
-	“name” : “Brandon Walsh”,
-	“street_address” : “123 My Street”,
-	“city” : “Charlottesville”,
-	“state” : “VA”,
-	“zip_code” : "22903"
+	"name" : "Brandon Walsh",
+	"street_address" : "123 My Street",
+	"city" : "Charlottesville",
+	"state" : "VA",
+	"zip_code" : "22903"
 }
+
+print(address["name"])
+print(address["street_address"])
+print(address["city"] + ", " + address["state"] + " " address["zip_code"])
---
+++ python/for_loops_ex.py
@@ -0,0 +1,10 @@
+fruits = ["kiwi", "strawberry", "plum"]
+results = []
+for item in fruits:
+    results.append(item)
+
+numbers = [1, 2, 3, 4, 5]
+results =[]
+
+for item in numbers:
+    results.append(item + 5)
